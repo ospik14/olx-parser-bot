c159349e92b6678cd206d9a164cdbee4d1abe1da
ospik14 <bliznakstap@gmail.com>
2026-02-12 21:53:21 +0200

    improved error handling and optimized the parser

--- app/parsers/olx_parser.py
+++ app/parsers/olx_parser.py
@@ -1,5 +1,5 @@
 from urllib.parse import urlparse, parse_qs, urlencode, urlunparse
-from playwright.async_api import async_playwright, Browser
+from playwright.async_api import Browser, TimeoutError, Error
 from schemas.advert import AdsResponse
 
 async def improve_link(url: str):
@@ -13,40 +13,58 @@ async def improve_link(url: str):
 
     return new_url
 
+async def intercept_route(route):
+    if route.request.resource_type in ['image', 'media', 'font']:
+        await route.abort()
+    else:
+        await route.continue_()
+
 async def search_for_ads(page_link, browser: Browser):
-    context = await browser.new_context()
+    try:
+        context = await browser.new_context()
 
-    page = await context.new_page()
-    await page.goto(page_link)
+        page = await context.new_page()
+        await page.route('**/*', intercept_route)
+        await page.goto(page_link, timeout=15000, wait_until='domcontentloaded')
 
-    await page.keyboard.press('Enter')
 
-    await page.wait_for_selector('[data-testid="listing-grid"]')
-    advert_grid = page.get_by_test_id('listing-grid').first
+        await page.wait_for_selector('[data-testid="listing-grid"]', timeout=10000)
+        advert_grid = page.get_by_test_id('listing-grid').first
 
-    cards = await advert_grid.get_by_test_id('l-card').all()
-    adverts: dict[int, AdsResponse] = {}
-    for card in cards:
-        await card.scroll_into_view_if_needed()
-        id = await card.get_attribute('id')
-        image = await card.locator('img').get_attribute('src')
-        title = await card.locator('h4').inner_text()
-        price = await card.locator('[data-testid="ad-price"]').inner_text()
-        location_date = await card.locator('[data-testid="location-date"]').inner_text()
-        link_part = await card.locator('[data-testid="ad-card-title"] a').get_attribute('href')
-        full_link = f'https://www.olx.ua{link_part}'
+        cards = await advert_grid.get_by_test_id('l-card').all()
+        adverts: dict[int, AdsResponse] = {}
+        for card in cards:
+            await card.scroll_into_view_if_needed()
+            id = await card.get_attribute('id')
+            image = await card.locator('img').first.get_attribute('src') or 'зображення немає'
+            title = await card.locator('h4').inner_text()
+            price = await card.locator('[data-testid="ad-price"]').inner_text()
+            location_date = await card.locator('[data-testid="location-date"]').inner_text()
+            link_part = await card.locator('[data-testid="ad-card-title"] a').get_attribute('href')
+            full_link = f'https://www.olx.ua{link_part}'
 
-        adverts[int(id)] = AdsResponse (                   
-                id = int(id),
-                title = title,
-                image_url = image,
-                price = price,
-                location_and_date = location_date,
-                advert_url = full_link
-            )
-            
-    await context.close()
+            adverts[int(id)] = AdsResponse (                   
+                    id = int(id),
+                    title = title,
+                    image_url = image,
+                    price = price,
+                    location_and_date = location_date,
+                    advert_url = full_link
+                )
+                
+        return adverts
+    
+    except TimeoutError:
+        print(f"Timeout: {page_link}")
+        return {}
+    except Error as e:
+        print(f"Playwright Error: {e}")
+        return {}
+    except Exception:
+        print(f"Code Error: {e}")
+        return {}
 
-    return adverts
+    finally:
+        await context.close()
     
     
--- app/services/main_process.py
+++ app/services/main_process.py
@@ -1,7 +1,7 @@
 import asyncio
 from repositories.ads import get_active_searches
 from services.advert_se import find_new_ads
-from core.database import AsyncSessionLocal, get_session ##
+from core.database import AsyncSessionLocal
 from playwright.async_api import async_playwright
 
 
@@ -12,13 +12,14 @@ async def pars_loop():
             async with async_playwright() as p:
                 browser = await p.chromium.launch(headless=True)
                 searches = await get_active_searches(db)
-                sem = asyncio.Semaphore(5)
 
+                sem = asyncio.Semaphore(5)
                 tasks = [
                     find_new_ads(sem, db, search.search_link, search.id, browser)
                     for search in searches
                 ]
         
                 await asyncio.gather(*tasks, return_exceptions=True)
+
                 
         await asyncio.sleep(120)
